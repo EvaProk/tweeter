d1c469cc5a676814583504f1ba19991e74405d52
Eva Prokhorenko <evaprokhorenko@gmail.com>
2021-09-07 23:52:05 +0000

    Create a script that makes Implement the charecter counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+
+// const $count = $("<output>");
+
+$(document).ready(function() {
+  console.log("I'm ready")
+  
+
+  $('#tweet-text').on("keyup", () => {
+    const max = 140;
+    let length = $('#tweet-text').val().length;
+    let chars = max - length;
+    $('#charNum').text(chars);
+    if (length >= max) {
+      $('#charNum').css("color", "red");
+    }
+
+  });
+});
