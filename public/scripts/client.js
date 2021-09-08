4667fababcfaea027bac53d4741275b712cc8135
Eva Prokhorenko <evaprokhorenko@gmail.com>
2021-09-08 22:52:41 +0000

    Add creating tweets dynamicly and edit formatting and css

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,97 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+
+$(() => {
+  // $("time.timeago").timeago();
+  // jQuery(document).ready(function() {
+  //   jQuery("time.timeago").timeago();
+  // });
+  
+
+  const tweetData = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+const renderTweets = function(tweets) {
+ 
+  const $newTweet = $('.new-tweet');
+    for(const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+
+      $newTweet.append($tweet)
+    }
+}
+
+ 
+  
+const createTweetElement = (data) =>{
+  return `<article class ="tweets-container">
+  <header class="userInfo">
+      <img class="photo" src="${data.user.avatars} ">
+      <div class="userName">
+      <span class="userN">${data.user.name} </span>
+      <span class="nickName">${data.user.handle} </span>
+    </div>
+  </header>
+
+  <div class="mainContent">
+    <p> ${data.content.text} </p>
+    <hr>
+  </div>
+
+  <footer class="tweetFooter">
+    <span class="date">${timeago.format(data.created_at)}</span>
+    <div> 
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-heart"></i>
+      <i class="fas fa-retweet"></i>
+    </div> 
+  </footer>
+
+</article>`
+
+}
+
+const $tweet = renderTweets(tweetData);
+  $('.new-tweet').append($tweet); 
+
+
+
+
+// const tweetData = {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@SirIsaac"
+//     },
+//   "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//   "created_at": 1461116232227  
+// }
+
+
+
+
+});
+
 
--- public/scripts/userInfo.js
+++ public/scripts/userInfo.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  // console.log("I'm ready")
+  
   
 
   console.log('test');
