c2ee1cc108be4cd6ff71b6853a8d72ba5eebb83e
Eva Prokhorenko <evaprokhorenko@gmail.com>
2021-09-09 01:05:43 +0000

    Add create tweets dynamicly, when submitting button is pressed

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,38 +6,38 @@ $(() => {
   // });
   
 
-  const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  // const tweetData = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ]
 
 const renderTweets = function(tweets) {
- 
-  const $newTweet = $('.new-tweet');
+  const $newTweet = $('.userTweets');
+  //$newTweet.empty();
     for(const tweet of tweets) {
       const $tweet = createTweetElement(tweet);
 
-      $newTweet.append($tweet)
+      $newTweet.prepend($tweet)
     }
 }
 
@@ -71,9 +71,48 @@ const createTweetElement = (data) =>{
 
 }
 
-const $tweet = renderTweets(tweetData);
-  $('.new-tweet').append($tweet); 
+// const $tweet = renderTweets(tweetData);
+//   $('.new-tweet').append($tweet); 
 
+  
+const $form = $('#new-tweet');
+  $form.on('submit', function(event) {
+    event.preventDefault();
+    console.log('the form has been submitted')
+    
+
+    const serializedData = $(this).serialize() 
+    console.log(serializedData);
+    if(serializedData.content.text === null){
+      alert("Fill in the form")
+    }
+
+    $.post('/tweets', serializedData, (response) => {
+      console.log(response)
+      loadTweets();
+    })
+
+  })
+
+  const loadTweets = () => {
+
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      dataType: 'json',
+      success: (tweets) => {
+        console.log("tweets", tweets)
+  
+        renderTweets(tweets);
+  
+      },
+      error: (err) => {
+        console.log(`errro: ${err}`)
+      }
+    })
+  }
+
+  renderTweets();
 
 
 
