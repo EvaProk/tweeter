e890573e12445e8ca81038aaabb8c7abce11143f
Eva Prokhorenko <evaprokhorenko@gmail.com>
2021-09-09 03:58:13 +0000

    Add a check if the form is emty, ot too long. Clean the form after submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,35 +1,6 @@
 
 $(() => {
-  // $("time.timeago").timeago();
-  // jQuery(document).ready(function() {
-  //   jQuery("time.timeago").timeago();
-  // });
   
-
-  // const tweetData = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ]
 
 const renderTweets = function(tweets) {
   const $newTweet = $('.userTweets');
@@ -71,28 +42,35 @@ const createTweetElement = (data) =>{
 
 }
 
-// const $tweet = renderTweets(tweetData);
-//   $('.new-tweet').append($tweet); 
+
 
   
 const $form = $('#new-tweet');
   $form.on('submit', function(event) {
     event.preventDefault();
     console.log('the form has been submitted')
-    
+    if($('#tweet-text').val() == ""){   // Checks if the form is empty, or not too long
+        alert("empty");
+        return
+      } else if($('#tweet-text').val().length >= 140){
+        alert("too many characters")
+        return
+      }
 
     const serializedData = $(this).serialize() 
     console.log(serializedData);
-    if(serializedData.content.text === null){
-      alert("Fill in the form")
-    }
-
+    // Validation form???? 
+    
     $.post('/tweets', serializedData, (response) => {
       console.log(response)
+      
       loadTweets();
+      $(this)[0].reset(); // cleans the form after submission
     })
 
   })
+
+  
 
   const loadTweets = () => {
 
@@ -115,18 +93,6 @@ const $form = $('#new-tweet');
   renderTweets();
 
 
-
-// const tweetData = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//   "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//   "created_at": 1461116232227  
-// }
 
 
 
