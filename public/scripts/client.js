d0208c539d0d17537386f70a5372fda04eefaee5
Eva Prokhorenko <evaprokhorenko@gmail.com>
2021-09-10 02:03:09 +0000

    Add styles for a devices with screen size >1024, fix cross-site scripting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,25 +1,21 @@
 
 $(() => {
 
-$( ".errorMessage" ).hide(); // Hides the errorMessage Div
+  $(".errorMessage").hide(); // Hides the errorMessage Div
 
-// loadTweets();
-
-
-
-const renderTweets = function(tweets) {
-  const $newTweet = $('.userTweets');
-    for(const tweet of tweets) {
+  const renderTweets = function(tweets) {
+    const $newTweet = $('.userTweets');
+    for (const tweet of tweets) {
       const $tweet = createTweetElement(tweet);
 
-      $newTweet.prepend($tweet)
+      $newTweet.prepend($tweet);
     }
-}
+  };
 
- 
-  
-const createTweetElement = (data) =>{
-  return `<article class ="tweets-container">
+
+
+  const createTweetElement = (data) => {
+    return `<article class ="tweets-container">
   <header class="userInfo">
       <img class="photo" src="${data.user.avatars} ">
       <div class="userName">
@@ -29,7 +25,7 @@ const createTweetElement = (data) =>{
   </header>
 
   <div class="mainContent">
-    <p> ${data.content.text} </p>
+    <p> ${escape(data.content.text)} </p>
     <hr>
   </div>
 
@@ -42,44 +38,51 @@ const createTweetElement = (data) =>{
     </div> 
   </footer>
 
-</article>`
+</article>`;
 
-}
+  };
+
+  const escape = function(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
 
 
-  
-const $form = $('#new-tweet');
+
+  const $form = $('#new-tweet');
   $form.on('submit', function(event) {
     event.preventDefault();
 
-    if($('#tweet-text').val() == ""){   // Checks if the form is empty, or not too long
-         $( ".errorMessage" ).slideDown();
-         $( ".errorText" ).text("Empty")
-        return
-      } else if($('#tweet-text').val().length >= 140){
-        $( ".errorMessage" ).slideDown();
-        $( ".errorText" ).text("Too long")
-        return
-      } else{
-        $( ".errorMessage" ).hide();
-      }
+    if ($('#tweet-text').val() == "") {   // Checks if the form is empty, or not too long
+      $(".errorMessage").slideDown();
+      $(".errorText").text("Empty");
+      return;
+    } else if ($('#tweet-text').val().length >= 140) {
+      $(".errorMessage").slideDown();
+      $(".errorText").text("Too long");
+      return;
+    } else {
+      $(".errorMessage").hide();
+    }
 
-    const serializedData = $(this).serialize() 
+    const serializedData = $(this).serialize();
     console.log(serializedData);
-    
+
     $.post('/tweets', serializedData, (response) => {
-      console.log(response)
-      
+      console.log(response);
+
       loadTweets();
+
       $(this)[0].reset(); // cleans the form after submission
-      $("#charNum").text(140);
+      $("#charNum").text(140); // resets the count number
 
-    })
+    });
 
-  })
+  });
 
-  
+
 
   const loadTweets = () => {
 
@@ -88,23 +91,18 @@ const $form = $('#new-tweet');
       method: 'GET',
       dataType: 'json',
       success: (tweets) => {
-        console.log("tweets", tweets)
-  
+        console.log("tweets", tweets);
+
         renderTweets(tweets);
-  
+
       },
       error: (err) => {
-        console.log(`errro: ${err}`)
+        console.log(`errro: ${err}`);
       }
-    })
-  }
+    });
+  };
 
   loadTweets();
-
-
-
-
-
 
 });
 
